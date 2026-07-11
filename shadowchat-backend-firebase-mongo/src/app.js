07dd8b88b02434974345df987a220553fe49c0b3
Sanu00 <sanusharma0045.v@gmail.com>
2026-07-11 19:30:12 +0530

    P

--- shadowchat-backend-firebase-mongo/src/app.js
+++ shadowchat-backend-firebase-mongo/src/app.js
@@ -1,62 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const helmet = require("helmet");
-// const morgan = require("morgan");
-// const path = require("path");
-// const routes = require("./routes");
-// const env = require("./config/env");
-// const { notFound, errorHandler } = require("./middlewares/errorHandler");
-// const { initializeFirebase } = require("./config/firebase");
-// const { ensureUploadsDir } = require("./services/storage.service");
-
-// function createApp() {
-//   initializeFirebase();
-//   ensureUploadsDir();
-
-//   const app = express();
-
-//   app.use(
-//     cors({
-//       origin(origin, callback) {
-//         if (!origin) return callback(null, true);
-//         if (env.clientOrigins.includes(origin)) return callback(null, true);
-//         if (origin.includes("localhost")) return callback(null, true);
-//         return callback(new Error("Not allowed by CORS"));
-//       },
-//       credentials: true
-//     })
-//   );
-
-//   app.use(helmet({
-//     crossOriginResourcePolicy: false
-//   }));
-//   app.use(morgan("dev"));
-//   app.use(express.json({ limit: "5mb" }));
-//   app.use(express.urlencoded({ extended: true }));
-
-//   app.use("/uploads", express.static(path.join(process.cwd(), env.uploadsDir)));
-
-//   app.get("/api/health", (req, res) => {
-//     res.json({
-//       success: true,
-//       message: "ShadowChat backend is running",
-//       data: {
-//         uptimeSeconds: Math.floor(process.uptime()),
-//         env: env.nodeEnv
-//       }
-//     });
-//   });
-
-//   app.use("/api", routes);
-
-//   app.use(notFound);
-//   app.use(errorHandler);
-
-//   return app;
-// }
-
-// module.exports = createApp;
-
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
@@ -123,6 +64,15 @@ function createApp() {
 
   app.use("/api", routes);
 
+  app.get("/", (req, res) => {
+    res.json({
+      success: true,
+      message: "Welcome to ShadowChat API 🚀",
+      status: "Running"
+    });
+  });
+
+  
   app.use(notFound);
   app.use(errorHandler);
 
